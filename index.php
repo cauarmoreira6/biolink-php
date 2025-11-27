721e0072d2b269e3124c45d7ec7aabdd2c654096
Cauã Ramalho Moreira <caua.r.moreira6@aluno.senai.br>
2025-11-27 09:26:19 -0300

    Criando loop com meus links pessoais.

--- index.php
+++ index.php
@@ -5,6 +5,14 @@ $bio = "Meu nome é Cauã, tenho 17 anos, estou cursando o 2ª do Ensino Médio,
     de segunda, terca e sexta, e eu faço SENAI de quarta e quinta o dia inteiro, faço curso
     de DEV (Desenvolvimento de Sistemas).";
 $imagem = "https://i.pinimg.com/1200x/2b/7a/01/2b7a01d9272a3f00a4b380110a87d3dd.jpg";
+
+$links = [
+    "Instagram" => "https://instagram.com/07.cauazimxy",
+    "WhatsApp" => "https://wa.me/17996021691",
+    "Meu Jogo Favorito" => "https://rachacuca.com.br/logica/problemas/",
+    "Música do Momento" => "https://www.youtube.com/watch?v=OzDkgc9bVhQ&list=RDOzDkgc9bVhQ&start_radio=1"
+];
+
 ?>
 <!DOCTYPE html>
 
@@ -24,6 +32,22 @@ $imagem = "https://i.pinimg.com/1200x/2b/7a/01/2b7a01d9272a3f00a4b380110a87d3dd.
         <p> <?php echo $bio; ?> </p>
         
         <div class="lista-links"></div>
+    </div>
+
+    <div class="lista-links">
+        <?php
+            foreach ($links as $texto => $url) {
+                $classe_extra = "";
+
+                if ($texto == "Instagram") 
+                    $classe_extra = "destaque";
+
+                // O PHP constrói o botão HTML
+                echo "<a href='$url' class='btn $classe_extra' target='_blank'>
+                $texto
+                </a>";
+            }
+        ?>
     </div>
 </body>
 </html>
